8f938ffc2af621ec45a51926f300f65421710b84
Ali Kabel <Ali@alis-macbook-pro.local>
2016-06-01 16:09:55 +0200

    Delete text

--- LOL/ViewController.swift
+++ LOL/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("@ Pull :D Seeking With Good Opptunity ^_^")
+        print("@ Pull :D Seeking With Good")
         print("@ Pull :D :D")
     }
 
